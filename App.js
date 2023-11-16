ecdd09b6145eb1033a4222956eba38631aa0317a
unknown <vanquyd647@gmail.com>
2023-11-16 23:32:47 +0700

    complete Buoi9.

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, Pressable, Alert } from 'react-native';
+import React from 'react';
+import HomeStack from './navigation/homeStack';
+import Login from './screens/Login';
+import Screen1 from './screens/Screen1';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  return(
+    <HomeStack/>
+  )
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
